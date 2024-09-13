4399ee9593e666c8f79a3167bcb94967bd27ac97
elect10 <wlgnsakswp@naver.com>
2024-09-14 04:29:56 +0900

    feat(main): design for desktop
    데스크탑 홈페이지 개발
    모바일은 버그 존재

--- archi_web/src/app/page.tsx
+++ archi_web/src/app/page.tsx
@@ -10,20 +10,18 @@ export default function Home() {
     exit: { opacity: 0, x: 50 },
   };
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between text-sm lg:flex">
+    <main className="flex flex-col items-center justify-between pl-16 pr-16 pt-10">
+      <div className="max-w-5xls z-10 w-full flex-grow items-center justify-between text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center text-pretty font-medium lg:static lg:w-auto lg:rounded-xl">
           archichatco@gmail.com &nbsp;
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white lg:static lg:size-auto lg:bg-none dark:from-black dark:via-black">
+        <div className="flex48 fixed bottom-0 left-0 w-full items-end justify-center bg-gradient-to-t from-white via-white text-xl font-semibold lg:static lg:size-auto lg:bg-none dark:from-black dark:via-black">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
           >
-            웹사이트 구성 변경중에 있습니다. 아직 내용 업데이트가 되지 않은점
-            양해부탁드립니다.{" "}
+            Team Archi{" "}
             <Image
               src="/logo.png"
               alt="Vercel Logo"
@@ -36,24 +34,38 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="flex flex-row">
-        <div className="relative z-[-1] flex place-items-start bg-black before:absolute before:h-[3s00px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40">
+      <div className="z-0 m-0 flex h-full w-full flex-grow flex-row">
+        <div className="z-[-1] flex w-8/12 -translate-y-32 place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-slate-200 before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-teal-200 after:via-teal-300 after:blur-2xl after:content-[''] sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-teal-700 before:dark:opacity-10 after:dark:from-teal-900 after:dark:via-[#318284] after:dark:opacity-40">
           <Image
-            className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-            src="/Mockup.svg"
+            className="z-0"
+            src="/mockup.png"
             alt="mockup pic"
-            width={500}
-            height={300}
+            width={30000}
+            height={3000}
             priority
           />
         </div>
 
-        <div className="relative z-[-1] flex place-items-start bg-black before:absolute before:h-[3s00px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40">
-          {/* 내용 */}
+        <div className="relative z-[-1] flex w-3/5 place-items-start before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-slate-200 before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-teal-200 after:via-teal-300 after:blur-2xl after:content-[''] sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-teal-700 before:dark:opacity-10 after:dark:from-teal-900 after:dark:via-[#318284] after:dark:opacity-40">
+          <div className="flex flex-col">
+            <div className="z-10 mt-40 pb-4 text-4xl font-semibold">
+              떠들자{" "}
+              <span className="pl-4 text-base font-medium"> by Archi</span>
+            </div>
+            <div className="z-10 mt-3 text-2xl font-semibold">
+              언어발달장애를 지닌 모든 사람들을 위한
+              <br /> AI-언어발달지도사.
+            </div>
+            <div className="z-10 mt-10 font-sans">
+              "떠들자"는 언어발달장애를 가진 아동들을 위한 <br />
+              AI 기반 언어 치료 서비스를 제공하는 것을 목표로 합니다. <br />
+              최신 AI 기술을 활용하여 언어 지연 치료를 지원합니다.
+            </div>
+          </div>
         </div>
       </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
+      <div className="grid flex-grow text-center lg:h-6 lg:w-4/6 lg:max-w-5xl lg:-translate-y-44 lg:grid-cols-4 lg:text-left">
         <Link href="/chitchat" legacyBehavior>
           <div
             //href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
@@ -62,13 +74,13 @@ export default function Home() {
             rel="noopener noreferrer"
           >
             <p className="m-0 max-w-[30ch] text-sm opacity-50"></p>
-            <h2 className="mb-3 text-2xl font-semibold">
+            <h2 className="mb-3 text-base font-semibold">
               떠들자{" "}
               {/* <span className="inline-block transition-transform group-hover:translate-x-5 motion-reduce:transform-none">
               -&gt;
             </span> */}
             </h2>
-            <p className="m-0 max-w-[30ch] text-sm opacity-50">
+            <p className="m-0 max-w-[30ch] text-xs opacity-50">
               Team Archi 의 언어치료 어플리케이션 떠들자.
             </p>
           </div>
@@ -82,13 +94,13 @@ export default function Home() {
             exit="exit"
             transition={{ duration: 1 }}
           >
-            <h2 className="mb-3 text-2xl font-semibold">
+            <h2 className="mb-3 text-base font-semibold">
               Archi{" "}
               {/* <span className="inline-block transition-transform group-hover:translate-x-5 motion-reduce:transform-none">
                 -&gt;
               </span> */}
             </h2>
-            <p className="m-0 max-w-[30ch] text-sm opacity-50">
+            <p className="m-0 max-w-[30ch] text-xs opacity-50">
               Team Archi 의 팀원 5명을 소개합니다.
             </p>
           </motion.div>
@@ -101,13 +113,13 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <h2 className="mb-3 text-2xl font-semibold">
+            <h2 className="text-bases mb-3 font-semibold">
               Journey{" "}
               {/* <span className="inline-block transition-transform group-hover:translate-x-5 motion-reduce:transform-none">
                 -&gt;
               </span> */}
             </h2>
-            <p className="m-0 max-w-[30ch] text-sm opacity-50">
+            <p className="m-0 max-w-[30ch] text-xs opacity-50">
               Team Archi 의 여정을 소개합니다.
             </p>
           </a>
@@ -119,15 +131,14 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <h2 className="mb-3 text-2xl font-semibold">
+            <h2 className="mb-3 text-base font-semibold">
               Thanks to{" "}
               {/* <span className="inline-block transition-transform group-hover:translate-x-5 motion-reduce:transform-none">
                 -&gt;
               </span> */}
             </h2>
-            <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-              Team Archi 의 &quot;떠들자 &quot;
-              <br /> 어플리케이션 제작에 도움을 주신 분들을 소개합니다.
+            <p className="m-0 max-w-[30ch] text-balance text-xs opacity-50">
+              &quot;떠들자&quot; 제작에 도움을 주신 분들을 소개합니다.
             </p>
           </a>
         </Link>
