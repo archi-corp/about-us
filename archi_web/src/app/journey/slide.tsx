0867cae5b9dfd1715f7bf1c2175f54b30e7b1aea
mnseok <min.seok@g.skku.edu>
2024-09-18 18:56:36 +0900

    fix(fe): fix slide component style

--- archi_web/src/app/journey/slide.tsx
+++ archi_web/src/app/journey/slide.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Image from "next/image";
 
 export default function Slide({
@@ -10,14 +12,14 @@ export default function Slide({
   subtitle: string;
 }) {
   return (
-    <div className="flex w-full items-center justify-center p-4 sm:w-1/2 md:w-1/3 lg:w-1/5">
-      <div className="group max-w-md overflow-hidden rounded-lg border border-transparent p-5 transition-colors hover:border-gray-300 hover:bg-gray-100">
-        {/* 5:3 비율의 썸네일 컨테이너 */}
+    <div className="w-full max-w-xs p-2">
+      <div className="group overflow-hidden rounded-lg border border-transparent p-3 transition-colors hover:border-gray-300 hover:bg-gray-100">
+        {/* 썸네일 컨테이너 */}
         <div className="relative h-0 w-full pb-[60%]">
-          <Image src={img} alt={title} layout="fill" objectFit="cover" />
+          <Image src={img} alt={title} fill style={{ objectFit: "cover" }} />
         </div>
         <h2 className="mt-4 text-center text-xl font-semibold">{title}</h2>
-        <p className="mt-2 line-clamp-1 text-center text-base text-gray-600">
+        <p className="mt-2 line-clamp-2 text-center text-base text-gray-600">
           {subtitle}
         </p>
       </div>
