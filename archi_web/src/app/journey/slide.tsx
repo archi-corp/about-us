e8db5afa3afa7d726ae2d162312bd79cb7d0afeb
강민석 <min.seok@g.skku.edu>
2024-09-20 02:15:03 +0900

    feat(fe): implement journey page (#8)
    * fix(fe): remove <a> tag in main page
    
    * feat(fe): add header
    
    * feat(fe): implement slide component
    
    * feat(fe): make carousel component
    
    * fix(fe): fix slide component style
    
    * feat(fe): apply carousel
    
    * fix(fe): fix eslint version
    
    * feat(fe): make detail part
    
    * feat(fe): complete detail part style
    
    * fix(fe): fix eslint version
    
    * fix(fe): add participants in detail
    
    * feat(fe): add content
    
    * fix(fe): fix font size
    
    * fix(infra): fixing typescript version
    
    * fix(fe): fix design consider responsive design and darkmode
    
    ---------
    
    Co-authored-by: rlalswla <133738655+elect10@users.noreply.github.com>

---
+++ archi_web/src/app/journey/slide.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import Image from "next/image";
+import { SlideDetail } from "./page";
+
+export default function Slide({
+  slide,
+  onClick,
+}: {
+  slide: SlideDetail;
+  onClick: () => void;
+}) {
+  const { title, content, img } = slide;
+  return (
+    <div className="w-full max-w-xs p-2" onClick={onClick}>
+      <div className="group overflow-hidden rounded-lg border border-transparent bg-transparent p-3 transition-colors hover:border-gray-900/20 hover:bg-gray-100/20">
+        {/* 썸네일 컨테이너 */}
+        <div className="relative h-0 w-full pb-[60%]">
+          <Image src={img} alt={title} fill style={{ objectFit: "cover" }} />
+        </div>
+        <div className="flex flex-col items-start">
+          <h2 className="mt-4 text-center text-xl font-semibold">{title}</h2>
+          <p className="mt-2 line-clamp-2 text-left text-base text-gray-600">
+            {content}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
