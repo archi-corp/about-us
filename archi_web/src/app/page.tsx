7a47bf4a77c010f3f0c4c49ef9c02292df78bea5
elect10 <wlgnsakswp@naver.com>
2024-09-20 23:04:21 +0900

    fix(main): change the content

--- archi_web/src/app/page.tsx
+++ archi_web/src/app/page.tsx
@@ -46,24 +46,30 @@ export default function Home() {
 
         <div className="relative z-[-1] flex h-full w-[550px] place-items-start pl-4 before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-slate-200 before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-teal-200 after:via-teal-300 after:blur-2xl after:content-[''] sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-teal-700 before:dark:opacity-10 after:dark:from-teal-900 after:dark:via-[#318284] after:dark:opacity-40">
           <div className="sm: flex flex-col md:flex-col lg:flex-col">
+
+
             <div className="z-10 hidden pb-4 pt-6 text-4xl font-semibold sm:mt-0 sm:block md:block lg:mt-40 lg:pt-0">
+
               떠들자{" "}
-              <span className="pl-4 text-base font-medium"> by Archi</span>
+              <span className="pl-4 text-base font-medium"> 12월중 베타테스트 예정</span>
+
+
             </div>
             <div className="z-10 mt-3 hidden text-2xl font-semibold sm:block">
-              언어발달장애를 지닌 모든 사람들을 위한 <br /> AI-언어발달지도사.
+              언어발달장애를 지닌 모든 사람들을 위한 <br/> AI-언어발달지도사.
             </div>
             <div className="z-10 mt-10 hidden font-sans sm:block">
               <span className="font-bold">떠들자</span>는 언어발달장애를 가진
-              아동들을 위한 <br />
-              AI 기반 언어 치료 서비스를 제공하는 것을 목표로 합니다. <br />
+              아동들을 위한 <br/>
+              AI 기반 언어 치료 서비스를 제공하는 것을 목표로 합니다. <br/>
               최신 AI 기술을 활용하여 언어 지연 치료를 지원합니다.
             </div>
           </div>
         </div>
       </div>
 
-      <div className="mb-32 grid flex-grow pt-20 text-center lg:mb-0 lg:h-6 lg:w-4/6 lg:max-w-5xl lg:-translate-y-28 lg:grid-cols-4 lg:text-left">
+      <div
+          className="mb-32 grid flex-grow pt-20 text-center lg:mb-0 lg:h-6 lg:w-4/6 lg:max-w-5xl lg:-translate-y-28 lg:grid-cols-4 lg:text-left">
         <Link href="/chitchat" legacyBehavior>
           <div className="group z-20 rounded-lg border border-transparent px-5 py-4 transition-colors lg:hover:border-gray-300 lg:hover:bg-gray-100 lg:hover:dark:border-neutral-700 lg:hover:dark:bg-neutral-800/30">
             <h2 className="mb-3 text-base font-semibold">떠들자 </h2>
