79eef1384cff27351706415ba27e14ef6dc5f229
racheliee <rachelie.dwg@gmail.com>
2024-09-20 01:05:54 -0700

    fix(member): fix alignment of text on small screens

--- archi_web/src/app/archi/yang/page.tsx
+++ archi_web/src/app/archi/yang/page.tsx
@@ -25,7 +25,7 @@ export default function Yang() {
                   ></Profile>
                   <div
                       className={
-                          "justify-left relative z-10 mx-32 flex flex-col items-start before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40"
+                          "justify-center lg:justify-start relative z-10 mx-4 lg:mx-32 flex flex-col items-center lg:items-start before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40"
                       }
                   >
                       <h1
