9b8eb20a0e02869ef22e589ced267bd120f5f532
elect10 <wlgnsakswp@naver.com>
2024-09-20 22:49:05 +0900

    fix(infra): set prettier

--- archi_web/src/app/page.tsx
+++ archi_web/src/app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
             {" "}
             <Image
               src="/logo.png"
-              alt="Vercel Logo"
+              alt="Archi Logo"
               className="dark:invert"
               width={100}
               height={24}
